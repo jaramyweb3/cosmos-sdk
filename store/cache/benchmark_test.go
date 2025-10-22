86cededbe6f114546eb61e5797437700a1b17110
Jammy Arkens <158516297+jaramyweb3@users.noreply.github.com>
2025-10-23 01:54:24 +0800

    Update benchmark_test.go

--- store/cache/benchmark_test.go
+++ store/cache/benchmark_test.go
@@ -25,10 +25,9 @@ func BenchmarkReset(b *testing.B) {
 	b.ReportAllocs()
 	mgr := freshMgr()
 
-	b.ResetTimer()
 	b.ReportAllocs()
 
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		mgr.Reset()
 		if len(mgr.caches) != 0 {
 			b.Fatal("Reset failed")
